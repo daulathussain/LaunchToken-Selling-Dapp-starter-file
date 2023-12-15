9792acedc10a9b835b606e10b3ce0892938ffff8
rameezraja0 <rameezraja1730482@gmail.com>
2023-12-15 23:59:53 +0500

    R

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,7 @@
 import React from "react";
 
-//INTERNAL IMPORT
-import { useStateContext } from "../Context/index";
-import {
-  About,
-  ArrowUp,
-  Banner, 
-  Blog, 
-  Client,
-  Contract,
-  Distribution,
-  Faq,
-  Footer,
-  Header,
-  Loader,
-  MobileApp,
-  Service,
-  Team,
-  Timeline,
-  TokenSale} from "../Components/index";
-
 const index = () => {
-  // const {Token_ICO, transferNativeToken } = useStateContext();
-  // return (
-  //   <div className="v_dark">
-  //     {/* <Header
-  //     address ={address}
-  //     setAddress = {setAddress}
-  //     connectWallet = {connectWallet}
-  //     /> */}
-
-  //     <Banner />
-  //     <Service />
-  //     <About/>
-  //     <TokenSale /> 
-  //     <Distribution />
-  //     <MobileApp />
-  //     <Team />
-  //     <Faq />
-  //     <Contract />
-  //     <Footer />
-  //   </div>
-  // );
+  return <div>index</div>;
 };
 
 export default index;
-
