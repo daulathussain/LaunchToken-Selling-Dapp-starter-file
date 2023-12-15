23f59bbc437b3dce2f7cfcc6b2e62ebe9fb1f327
rameezraja0 <rameezraja1730482@gmail.com>
2023-12-16 01:23:19 +0500

    R

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,5 @@ import React from "react";
 
 const index = () => {
   return <div>INDEXX</div>;
-  return <div>INDEXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXXX</div>;
 };
-  return <div>Rameez</div>
 export default index;
