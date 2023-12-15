0c2087b6fc21fd4db1feac2455ca4011653288c8
rameezraja0 <rameezraja1730482@gmail.com>
2023-12-15 23:35:01 +0500

    commit

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,48 @@
 import React from "react";
 
+//INTERNAL IMPORT
+import { useStateContext } from "../Context/index";
+import {
+  About,
+  ArrowUp,
+  Banner, 
+  Blog, 
+  Client,
+  Contract,
+  Distribution,
+  Faq,
+  Footer,
+  Header,
+  Loader,
+  MobileApp,
+  Service,
+  Team,
+  Timeline,
+  TokenSale} from "../Components/index";
+
 const index = () => {
-  return <div>index</div>;
+  // const {Token_ICO, transferNativeToken } = useStateContext();
+  // return (
+  //   <div className="v_dark">
+  //     {/* <Header
+  //     address ={address}
+  //     setAddress = {setAddress}
+  //     connectWallet = {connectWallet}
+  //     /> */}
+
+  //     <Banner />
+  //     <Service />
+  //     <About/>
+  //     <TokenSale /> 
+  //     <Distribution />
+  //     <MobileApp />
+  //     <Team />
+  //     <Faq />
+  //     <Contract />
+  //     <Footer />
+  //   </div>
+  // );
 };
 
 export default index;
+
---
+++ public/index.js
@@ -0,0 +1,36 @@
+import About from './About';
+import ArrowUp from './ArrowUp';
+import Banner from './Banner';
+import Blog from './Blog';
+import Client from './Client';
+import Contract from './Contract';
+import Distribution from './Distribution';
+import Faq from './Faq';
+import Footer from './Footer';
+import Header from './Header';
+import Loader from './Loader';
+import MobileApp from './MobileApp';
+import Service from './Service';
+import Team from './Team';
+import Timeline from './Timeline';
+import TokenSale from './TokenSale'; 
+
+
+export {
+    About,
+    ArrowUp,
+    Banner, 
+    Blog, 
+    Client,
+    Contract,
+    Distribution,
+    Faq,
+    Footer,
+    Header,
+    Loader,
+    MobileApp,
+    Service,
+    Team,
+    Timeline,
+    TokenSale
+}
